bfef8b2821b882c8bd4f911f1694492f3dc42889
Omar Insurriaga <oinsur@gmail.com>
2022-05-02 21:26:48 -0600

    guess logic almost done

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,15 +2,15 @@ import React, {useContext} from 'react';
 import {AppContext} from "../App";
 
 function Letter({ letterPos, attemptValue }) {
-  const {board, rightWord} = useContext(AppContext); 
+  const {board, rightWord, currentAttempt } = useContext(AppContext); 
   const letter = board[attemptValue][letterPos];
 
   const correct = rightWord[letterPos] === letter;
   const almost = !correct && letter !== "" && rightWord.includes(letter);
 
-  const letterState = correct ? "correct" : almost ? "almost" : "error";
+  const letterState = currentAttempt.attempt > attemptValue && (correct ? "correct" : almost ? "almost" : "error");
 
-  return <div className='letter' id={letterState}>{letter}</div>
+  return <div className='letter' id={letterState}>{" "} {letter}</div>
   
 }
 
